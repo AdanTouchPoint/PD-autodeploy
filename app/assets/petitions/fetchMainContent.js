a566c5656b629a90a4a980a73b9e9e49f1e62cde
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2025-08-04 18:53:57 -0600

    fixed fetchMAinContent request

--- app/assets/petitions/fetchMainContent.js
+++ app/assets/petitions/fetchMainContent.js
@@ -1,19 +1,14 @@
-const fetchMainContent = async (backendURLBase, id, clientId, campaignType) => {
-    const requestOptions = {
-        method: "GET",
-        redirect: 'follow',
-    };
-    const url = `${backendURLBase}/campaignContentId?clientId=${clientId}&id=${id}&type=${campaignType}`;
-    const pageData = await fetch(url, requestOptions, { cache: 'no-store' });
-
-    if (!pageData.ok) {
-        throw new Error(`Failed to fetch main content: ${pageData.status} ${pageData.statusText}`);
+const fetchMainContent = async (backendURLBase,id,clientId, campaignType) => {
+    
+    const pageData = await fetch(`${backendURLBase}/campaignContentId?clientId=${clientId}&id=${id}&type=${campaignType}`, { cache: 'no-store' })
+    if (pageData.ok === false) {
+        return false
     }
-
-    const data = await pageData.json();
-    console.log('Fetched main content data:', data.data.docs[0]);
-    return data;
-};
+   
+    const data = await pageData.json()
+     console.log("fetchMainContent", data.data.docs[0].mainform);
+    return data 
+}
 
 export {
     fetchMainContent
